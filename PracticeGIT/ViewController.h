49d020c16f4c8ffde7491ec3661af1f54edaea79
nikhithak <nikhithak24@gmail.com>
2016-05-17 18:29:53 -0400

    Initial Commit

---
+++ PracticeGIT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PracticeGIT
+//
+//  Created by Madhu kiran Vemula on 5/17/16.
+//  Copyright © 2016 Nikhitha Akula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PracticeGIT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PracticeGIT
+//
+//  Created by Madhu kiran Vemula on 5/17/16.
+//  Copyright © 2016 Nikhitha Akula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
